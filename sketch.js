e436a47007e851c5538d453e20a5480136a27fbe
Saarthak <sasmitadiabagh@gmail.com>
2020-07-26 20:10:29 +0530

    C29

--- sketch.js
+++ sketch.js
@@ -2,8 +2,13 @@ function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
 }
+string01 = new String1(Object.body,{x:100y:200});
+object1 = new Object(100,200,20,20);
 
 function draw() {
   background(255,255,255);  
   drawSprites();
+  //object1.display();
+  //string01.display();
+
 }
